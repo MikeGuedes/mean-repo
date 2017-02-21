3660eed1c8ca01b50409a2ef798e9fff8739c692
Michael Guedes <michaelguedes2@gmail.com>
2017-02-20 23:46:23 -0300

    Configuração de variáveis de ambiente e middleware de acesso à conteúdo estárico

--- model/categoria.js
+++ model/categoria.js
@@ -1,11 +1,20 @@
 //  Dependencias
 var restfull = require('node-restful');
 var mongoose = restfull.mongoose;
+// var mongoose = require('mongoose');
 
 //  Schema
 var categoriaSchema = new mongoose.Schema({
-    nome: String
+    nome: {
+        type: String,
+        require: true
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 //  Return Schema
 module.exports = restfull.model('Categoria', categoriaSchema);
+// var Categorie = module.exports = mongoose.model('Categorie', CategorieSchema);
